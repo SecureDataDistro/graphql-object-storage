ff6920c2b580a16f9294865ec6f7f8e395eb0b50
j.taylor <j.taylor@orbisops.com>
2023-07-10 16:50:32 +0000

    updating server to add health check

--- src/object-storage/server/index.ts
+++ src/object-storage/server/index.ts
@@ -4,6 +4,14 @@ import { GraphQLError } from 'graphql';
 import {JWT} from "@hub/jwt"
 import { Authorizer } from "@hub/iam";
 
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { config } from "process";
+
 export interface GraphqlServerConfig {
     serverPort: number
     publicKeyEndpoint: string
@@ -16,21 +24,28 @@ export interface AuthContext extends BaseContext {
 
 export async function StartApolloStandaloneServer(config: GraphqlServerConfig, typeDefs: any, resolvers: any) {
     
+    const app = express();
+    const httpServer = http.createServer(app);
     
     const server = new ApolloServer<AuthContext>({
         //schema: buildSubgraphSchema({ typeDefs, resolvers }),
         typeDefs,
-        resolvers
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
     });
 
+    await server.start();
+
     const auth = new Authorizer(config.publicKeyEndpoint);
-    
-    // Passing an ApolloServer instance to the `startStandaloneServer` function:
-    //  1. creates an Express app
-    //  2. installs your ApolloServer instance as middleware
-    //  3. prepares your app to handle incoming requests
-    const { url } = await startStandaloneServer(server, {
-        listen: { port: config.serverPort },
+
+    app.use(
+      '/graphql',
+      cors<cors.CorsRequest>(),
+      // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
+      bodyParser.json({ limit: '50mb' }),
+      // expressMiddleware accepts the same arguments:
+      // an Apollo Server instance and optional configuration options
+      expressMiddleware<AuthContext>(server, {
         context: async ({ req, res }) => {
             // Get the user token from the headers.
             const token: string = req.headers["x-hasura-token"] as string || '';
@@ -70,7 +85,16 @@ export async function StartApolloStandaloneServer(config: GraphqlServerConfig, t
         
             // Add the user to the context
             return { jwt };
-          },
+          }
+        }
+      ),
+    );
+
+    app.get('/health', (req, res) => {
+      res.status(200).send('ok');
     });
     
+    // Modified server startup
+    await new Promise<void>((resolve) => httpServer.listen({ port: config.serverPort }, resolve));
+    console.log(`🚀 Server ready at http://localhost:4000/`);
 }
